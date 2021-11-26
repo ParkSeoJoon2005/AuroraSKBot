911220ed660d78b1bb5d69f219a2d30bf4e60c8c
Park Seo Joon <87398748+ParkSeoJoon2005@users.noreply.github.com>
2021-11-26 19:24:29 +0530

    Create github.py

---
+++ DaisyX/modules/github.py
@@ -0,0 +1,7 @@
+__help__ = """
+✪ /git: Returns info about a GitHub user or organization.[](https://telegra.ph/file/6dabe6c48e206fe579a12.jpg)
+✪ /repo: Return the GitHub user or organization repository list (Limited at 40).
+
+GitHub, Inc. is a provider of Internet hosting for software development and version control using Git. It offers the distributed version control and source code management (SCM) functionality of Git, plus its own features. It provides access control and several collaboration features such as bug tracking, feature requests, task management, continuous integration and wikis for every project. Headquartered in California, it has been a subsidiary of Microsoft since 2018.
+"""
+__mod_name__ = "Github"
