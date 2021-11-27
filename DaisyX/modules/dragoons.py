075a62a34b57a2d9bf5c0b9dd6b4fdbf4f18cf1f
Park Seo Joon <87398748+ParkSeoJoon2005@users.noreply.github.com>
2021-11-27 06:53:33 +0530

    Create dragoons.py

---
+++ DaisyX/modules/dragoons.py
@@ -0,0 +1,32 @@
+__help__ = """
+**For DRAGONS Of The Project**
+
+/addsudo
+/addpiro - To add a sudo user
+/addsupport - to add a support user
+/adddemon - same as support
+/addwhitelist - to add a whitelistuser
+/addwolf - same as whitelist
+/removesudo - remove sudo
+/removedragon - same as sudo
+/removesupport
+/removedemons
+/removetiger
+/removewhitelist
+/removewolf
+
+
+/whitelist
+/wolves
+/tigers
+/supportlist
+/demons
+/sudolist
+/dragons
+/devlist  - To get list of dragons
+
+/broadcast - to broadcast a msg
+/gban - global ban a user
+
+"""
+__mod_name__ = "Dragons"
