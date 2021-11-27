8c5b9dcc8cdfc76dd09dcb33aa9a5d5cf6763845
Park Seo Joon <87398748+ParkSeoJoon2005@users.noreply.github.com>
2021-11-27 05:54:24 +0530

    Create covidd.py

---
+++ DaisyX/modules/covidd.py
@@ -0,0 +1,12 @@
+__help__ = """
+
+**Commands**
+     
+• /covid - To Get Global Stats of Covid.
+• /covid COUNTRY - To Get Stats of A Single Country.
+• /corona - Same as /covid
+
+"""
+
+__mod_name__ = "Covid19"
+
